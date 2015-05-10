07683e639dee50d0294a67f85367fbc533d02ce0
root <simon.jiyou@gmail.com>
2015-05-09 21:37:35 -0400

    add POST test

--- routes/schools.js
+++ routes/schools.js
@@ -2,13 +2,22 @@ var School = require('../models/school');
 var express = require('express');
 var router = express.Router();
 
-router.route('/schools').get(function(req, res) {
-  School.find(function(err, schools) {
-    if (err) {
-      return res.send(err);
-    }
-    res.json(schools);
+router.route('/schools')
+  .get(function(req, res) {
+    School.find(function(err, schools) {
+      if (err) {
+        return res.send(err);
+      }
+      res.json(schools);
+    });
+  })
+  .post(function(req,res){
+    var school = new School(req.body);
+    school.save(function(err){
+      if(err)
+	res.send(err);
+      res.send({message:'School Added'});
+    });
   });
-});
 
 module.exports=router;
