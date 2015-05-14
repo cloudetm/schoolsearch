280f159531403aeaafb2edd82f18c4de055cf254
root <simon.jiyou@gmail.com>
2015-05-13 20:28:55 -0400

    bug fix for data import tool(postcode.js)

--- postcode.js
+++ postcode.js
@@ -1,18 +1,7 @@
 var fs = require('fs');
-var http = require('http');
-
+//var http = require('http');
+var hq = require('hyperquest');
 var file = process.argv[2];
-
-var headers = {
-  'Content-Type': 'application/json'
-};
-var options = {
-  host: 'localhost',
-  port: 8000,
-  path: '/api/postcodes',
-  method: 'POST',
-  headers: headers
-}
 
 function readFileToMongo(file) {
   fs.readFile(file, "utf-8", function(err, data) {
@@ -31,35 +20,92 @@ function readFileToMongo(file) {
           region: str[4] 
         };
       });
-    var json = JSON.parse(JSON.stringify(tmpFile));
+    console.log(JSON.stringify(tmpFile));
 
-    json.forEach(function(item) {
-      var req = http.request(options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log("body: " + chunk);
-        });
+    //var json = JSON.parse(JSON.stringify(tmpFile));
+
+    /*
+    var opts, ws, body, buffer,
+    url = "http://127.0.0.1:8000/api/postcodes";
+    
+    for (var i = 0; i < 2000; i ++) {
+      body = JSON.stringify(json[i]); 
+      opts = {
+      	headers: {
+      	  'Content-Type': 'application/json',
+      	  'Content-Length': body.length
+      	}
+      };       
+      req = hq.post(url, opts);
+      req.end(body);
+
+      buffer = '';
+
+      req.on('data', function(chunk) {
+        return buffer += chunk;
       });
-      req.write(JSON.stringify(item)); 
-      req.end();
-      console.log(JSON.stringify(item));
+
+      req.on('error', function(err) {
+        console.log(err);
+      });
+
+      req.on('response', function(res) {
+        var err = null;     
+        
+        if (res.statusCode >= 400) {
+          err = new Error('Bad statusCode in response: '+ res.statusCode);
+          err.statusCode = res.statusCode;
+        }
+        
+        res.body = buffer;
+        console.log(res.body);        
+      });
+    };
+    */
+
+    /*
+    json.forEach(function(item) {
+      var opts, req, body, buffer,
+      url = "http://127.0.0.1:8000/api/postcodes";
+
+      body = JSON.stringify(item); 
+      console.log(body);
+      opts = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': body.length
+        }
+      };       
+      req = hq.post(url, opts);
+      req.end(body);
+
+      buffer = '';
+
+      req.on('data', function(chunk) {
+        return buffer += chunk;
+      });
+
+      req.on('error', function(err) {
+        console.log(err);
+      });
+
+      req.on('response', function(res) {
+        var err = null;
+      
+        //console.log(ws.response);
+        //res = ws.response;
+        
+        if (res.statusCode >= 400) {
+          err = new Error('Bad statusCode in response: '+ res.statusCode);
+          err.statusCode = res.statusCode;
+        }
+        
+        res.body = buffer;
+        console.log(res.body);        
+      });
+    
     });
-
-    //for (var i in pureJson) {
-    //  console.log(pureJson[i]);
-    //  req.write(pureJson[i]); 
-    //};
-
-    //pureJson.map(function(item) {
-    //  console.log(item);
-    //});
-    //for (var i = 0; i < json.length; i++) {
-    //  console.log(JSON.stringify(json[i]));
-      //req.write(JSON.stringify(json[i]));
-    //}
-
-    //req.end();
-    //console.log(pureJson);
+    */
   });
 }
 
