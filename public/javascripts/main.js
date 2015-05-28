6acd2b4d6c52d9dbb5198b5d00f6f66f4ced2023
System Administrator <simon.jiyou@gmail.com>
2015-05-27 23:57:12 -0600

    UI

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -64,7 +64,18 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
   $scope.showListBottomSheet = function($event) {
     $scope.alert = '';
     $mdBottomSheet.show({
-      template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
+      template: '<md-bottom-sheet class="md-list md-has-header"> 
+                 <md-subheader>Settings</md-subheader>
+                  <md-list>
+                    <md-item ng-repeat="item in items">
+                      <md-item-content md-ink-ripple flex class="inset">
+                        <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> 
+                          <span class="md-inline-list-icon-label">{{ item.name }}</span> 
+                        </a>
+                      </md-item-content> 
+                    </md-item> 
+                  </md-list>
+                </md-bottom-sheet>',
       controller: 'ListBottomSheetCtrl',
       targetEvent: $event
     }).then(function(clickedItem) {
